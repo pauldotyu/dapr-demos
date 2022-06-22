9ba70699b6702e2cd76e6fa61718a32353cfe3bb
Paul Yu <paul.d.yu@gmail.com>
2022-06-22 08:05:10 -0700

    Update response messages

--- hello-service/app.js
+++ hello-service/app.js
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 
 app.get("/sayHello", (_, res) => {
-  let hello = "";
-  for (const letter of "hello") {
-    const isUpperCase = Math.round(Math.random());
-    hello += isUpperCase ? letter.toUpperCase() : letter;
-  }
+  let hello = "hello";
+  // for (const letter of "hello") {
+  //   const isUpperCase = Math.round(Math.random());
+  //   hello += isUpperCase ? letter.toUpperCase() : letter;
+  // }
   console.log(`Sending: ${hello}`);
   res.send(hello);
 });
